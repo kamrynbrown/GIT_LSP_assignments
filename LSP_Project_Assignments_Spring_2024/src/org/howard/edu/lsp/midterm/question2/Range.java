3c19de5affe8fd405e58bf33c7e38b2ec3be667f
Kamryn Brown <kamrynbrown111@gmail.com>
2024-03-14 20:40:12 -0400

    changed interface file

--- LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/midterm/question2/Range.java
+++ LSP_Project_Assignments_Spring_2024/src/org/howard/edu/lsp/midterm/question2/Range.java
@@ -1,29 +1,14 @@
 package org.howard.edu.lsp.midterm.question2;
 
-/**
- * Interface representing a range of integers.
- */
 public interface Range {
-    /**
-     * Checks if the given value is within the range.
-     *
-     * @param value the value to check
-     * @return true if the value is within the range, false otherwise
-     */
-    boolean contains(int value);
+	// Returns true if v is ≥ lower bound and ≤ upper bound and false otherwise
+	public boolean contains( int value ); 
 
-    /**
-     * Checks if there is any overlap between this range and another range.
-     *
-     * @param other the other range to check for overlap
-     * @return true if there is an overlap, false otherwise
-     */
-    boolean overlaps(Range other);
+	// Returns true if the receiver contains at least 
+	// one value in common with other, and false otherwise
+	public boolean overlaps( Range other );
 
-    /**
-     * Returns the number of integers in the range.
-     *
-     * @return the size of the range
-     */
-    int size();
+	// // Returns the number of integers in the range
+	public int size(); 
 }
+
